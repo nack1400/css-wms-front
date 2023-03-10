60f42debaa345c6196df9e95bcbd3a9de99be887
nack1400 <nack1400@naver.com>
2023-03-09 16:08:51 -0800

    css-wms-init

---
+++ src/components/Image/ImageFallback.jsx
@@ -0,0 +1,28 @@
+import Image, { ImageProps } from 'next/image'
+import { useEffect, useState } from 'react'
+
+// Source: https://stackoverflow.com/questions/66949606/what-is-the-best-way-to-have-a-fallback-image-in-nextjs
+export default function ImageFallback(props) {
+  const {
+    src,
+    fallbackSrc,
+    ...rest
+  } = props
+
+  const [imgSrc, setImgSrc] = useState(src)
+
+  useEffect(() => {
+    setImgSrc(src)
+  }, [src])
+  return (
+    <Image
+      /* eslint-disable-next-line react/jsx-props-no-spreading */
+      {...rest}
+      alt={rest.alt ?? ''}
+      src={imgSrc}
+      onError={() => {
+        setImgSrc(fallbackSrc)
+      }}
+    />
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import '@styles/globals.scss'
+// Next.js allows you to import CSS directly in .js files.
+// It handles optimization and all the necessary Webpack configuration to make this work.
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { SSRProvider } from 'react-bootstrap'
+import { ProgressBar } from '@components/ProgressBar'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// You change this configuration value to false so that the Font Awesome core SVG library
+// will not try and insert <style> elements into the <head> of the page.
+// Next.js blocks this from happening anyway so you might as well not even try.
+// See https://fontawesome.com/v6/docs/web/use-with/react/use-with#next-js
+config.autoAddCss = false
+
+function MyApp({ Component, pageProps }) {
+  // In server-side rendered applications, a SSRProvider must wrap the application in order
+  // to ensure that the auto-generated ids are consistent between the server and client.
+  // https://react-bootstrap.github.io/getting-started/server-side-rendering/
+  return (
+    <SSRProvider>
+      <ProgressBar />
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </SSRProvider>
+  )
 }
+
+export default MyApp
